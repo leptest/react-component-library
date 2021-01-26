feedbf54dcad1b24c15c108f7e703285c8c88782
Daniel Chapman <leptest@gmail.com>
2021-01-26 22:57:32 +0000

    added missing examples code from cli tool

---
+++ example/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { MagicButton, SpecialPanel } from '@leptest/rcl'
+import '@leptest/rcl/dist/index.css'
+
+const App = () => {
+  return <div>
+      <h1>My Example Demo Project</h1>
+      <SpecialPanel text="Create React Library Example 😄" />
+      <MagicButton text="Magic Button Text" />
+  </div>
+}
+
+export default App
