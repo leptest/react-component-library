42404044ad16f3ea996ae8a526b4ffbe948fdde5
Daniel Chapman <leptest@users.noreply.github.com>
2023-12-02 15:29:18 +0000

    added theme to styled button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import styled from 'styled-components'
+import React, {ReactNode} from 'react';
+import styled, { ThemeProvider, css } from 'styled-components';
 
-interface Props {
-  text: string
+interface MagicButtonProps {
+  type: string,
+  children: ReactNode
 }
 
 const Button = styled.a`
+	${({ theme }) => css`
+		font-size: ${theme.maxWidth};
+	`}
   display: inline-block;
   border-radius: 3px;
   padding: 1rem 2rem;
-  background: green;
+  background: purple;
   color: white;
   border: 2px solid red;
 
@@ -18,8 +22,19 @@ const Button = styled.a`
   }
 `
 
-const MagicButton = ({ text }: Props) => {
-  return <Button href="/my-page">{text}</Button>
+const theme = {
+	maxWidth: '50px',
 }
+
+const MagicButton = ({children, type}: MagicButtonProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button href="/my-page" type={type}>{children} - test</Button>
+    </ThemeProvider>
+  )
+}
+// const MagicButton: React.FC = ({ type }: Props) => {
+//   return <Button href="/my-page" type={type}>sdgsdg</Button>
+// }
 
 export default MagicButton
